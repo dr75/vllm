733fc5315ac2c3e89413b06e784f3ab4af9e9563
Marko Rosenmueller <5467316+dr75@users.noreply.github.com>
2026-02-17 09:17:07 +0000

    usage reporting
    Signed-off-by: Marko Rosenmueller <5467316+dr75@users.noreply.github.com>

--- patch-tests.sh
+++ patch-tests.sh
@@ -2,3 +2,6 @@
 
 # voxtral segmentation
 pytest -v tests/entrypoints/test_speech_to_text_segments.py
+
+# anthropic cache salting and cached token reporting
+pytest -v tests/entrypoints/openai/test_messages.py
